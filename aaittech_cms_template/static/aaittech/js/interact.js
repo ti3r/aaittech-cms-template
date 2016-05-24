3cd9753ed95eef8ee4d46ce94f6e41106c852a12
Alexandro Blanco <ti3r.bubblenet@gmail.com>
2016-05-24 16:28:21 -0500

    Actualizando el templete para mejorar el menu y minimizar el codigo javascript que se importa

--- aaittech_cms_template/static/aaittech/js/interact.js
+++ aaittech_cms_template/static/aaittech/js/interact.js
@@ -1,114 +1,9 @@
-$(document).ready(function(){
-	$("#closenav").hide();
-	$(".viewpopup").hide();
-	
-	var closenav =$("#closenav"),
-		navbtn =$("#navbtn"),
-		navigation =$(".fullnavigation"),
-		navigationbtns =$(".fullnavigation ul li a")
-		imgdisplay=$(".imgdisplay"),
-		contentbox =$(".contentbox"),
-	
-	navbtn.click(function() {
-		navigation.animate({right:'0px'}, 1000, 'easeOutBack');
-		navbtn.fadeOut("500");
-		closenav.delay(1000).fadeIn("100");
-	});
-	
-	closenav.click(function() {
-		navigation.animate({right:'-200px'}, 1000);
-		navbtn.delay(1000).fadeIn("200");
-		closenav.fadeOut("800");
-	});	
-	
-	navigationbtns.click(function() {
-		navigation.animate({right:'-200px'}, 1000);
-		navbtn.delay(1000).fadeIn("200");
-		closenav.fadeOut("800");
-	});
-	
-	contentbox.mouseenter(function() {
-		$(this).children().first().next().find("p").removeClass("animated fadeOutDown");
-		$(this).children().first().next().find("p").addClass("animated fadeInDown");
-	});
-	
-	contentbox.mouseleave(function() {
-		$(this).children().first().next().find("p").removeClass("animated fadeInDown");
-		$(this).children().first().next().find("p").addClass("animated fadeOutDown");
-	});
-	
-	
-});
-
-
-$(window).load(function(){
-	//Fade in Different Text
-	var textlist = new Array("Reggae Music", "Skimboarding","The Beach", "Simple Design");
-	var timer;
-	
-	function textFade (index){
-		$("#update").hide().html(textlist[index])
-		.fadeIn(800);
-		
-		index++;
-		timer = setTimeout(function(){
-			textFade(index % textlist.length);
-			}, 2000);
-		}
-$(document).ready(function(){
-		textFade(0);
-	});
-});
-
-
-
 $(function() {
 
-    function scroll(direction) {
-
-        var scroll, i,
-                positions = [],
-                here = $(window).scrollTop(),
-                collection = $('.section');
-
-        collection.each(function() {
-            positions.push(parseInt($(this).offset()['top'],10));
-        });
-
-        for(i = 0; i < positions.length; i++) {
-            if (direction == 'next' && positions[i] > here) { scroll = collection.get(i); break; }
-            if (direction == 'prev' && i > 0 && positions[i] >= here) { scroll = collection.get(i-1); break; }
-        }
-
-        if (scroll) {
-            $.scrollTo(scroll, {
-                duration: 1000       
-            });
-        }
-
-        return false;
-    }
-
-    $("#next,#prev").click(function() {        
-        return scroll($(this).attr('id'));        
+    $(".nav a").on("click", function(){
+       $(".nav").find(".active").removeClass("active");
+       $(this).parent().addClass("active");
     });
-
-// Added code
-// Register keypress events for the current page
-$(document).keypress(function(e) {
-	switch(e.keyCode) { 
-		// User pressed "up" arrow
-      	case 38:
-    		return scroll("prev");
-     	break;
-   		// User pressed "down" arrow
-     	case 40:
-       		return scroll("next");
-    	break;
-	}
-});
-
-// End added code
 
     $(".scrolltoanchor").click(function() {
         $.scrollTo($($(this).attr("href")), {
@@ -117,6 +12,33 @@ $(document).keypress(function(e) {
         return false;
     });
 
+    // Cache selectors
+    var topMenu = $("#collapse-menu"),
+    topMenuHeight = topMenu.outerHeight()+15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+    $(window).scroll(function(){
+        // Get container scroll position
+       var fromTop = $(this).scrollTop()+topMenuHeight;
+       // Get id of current scroll item
+       var cur = scrollItems.map(function(){
+         if ($(this).offset().top < fromTop)
+           return this;
+       });
+        // Get the id of the current element
+       cur = cur[cur.length-1];
+       var id = cur && cur.length ? cur[0].id : "";
+        // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href='#"+id+"']").parent().addClass("active");
+    });
 });
 
 
